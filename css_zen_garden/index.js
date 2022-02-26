787d36938975bcf3c7de0a1298ee9af5042e8630
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-02-26 02:26:27 -0500

    css zen javascript update

--- css_zen_garden/index.js
+++ css_zen_garden/index.js
@@ -11,59 +11,48 @@ list[1].hidden = true;
 list[2].hidden = true;
 list[3].hidden = true;
 
+let nextCount = 0
 
-for(let i = 0; i < list.length; i++) {
-    let nextCount = 0
-    next.addEventListener('click', function(e){
-        e.preventDefault()
-        let countUp = nextCount
-        countUp += 1
-        console.log(nextCount)
-        if(navigations[0].style.display === 'none') {
-            navigations[0].style.display = 'inline-block';
-        }
-        if(navigations[0].style.display !== 'none' && countUp === 1) {
-            list[0].hidden = true;
-            list[1].hidden = false;
-        } if(countUp === 2) {
-            list[1].hidden = true;
-            list[2].hidden = false;
-        } if(countUp === 3) {
-            list[2].hidden = true;
-            list[3].hidden = false;
-        } if(countUp === 0) {
-            navigations[0].style.display === 'none'
-            list[0].hidden = false;
-            list[1].hidden = true;
-        }
-        nextCount = countUp
-    })
+function scrollLinks(){
+    if(nextCount === 1) {
+        navigations[0].style.display = 'inline-block'
+        list[0].hidden = true;
+        list[1].hidden = false;
+        list[2].hidden = true;
+        list[3].hidden = true;
+    } 
+    if(nextCount === 2) {
+        list[0].hidden = true;
+        list[1].hidden = true;
+        list[2].hidden = false;
+        list[3].hidden = true;
+    } 
+    if(nextCount === 3) {
+        list[0].hidden = true;
+        list[1].hidden = true;
+        list[2].hidden = true;
+        list[3].hidden = false;
+    } 
+    if(nextCount === 0) {
+        navigations[0].style.display === 'none'
+        list[0].hidden = false;
+        list[1].hidden = true;
+        list[2].hidden = true;
+        list[3].hidden = true;
+    }
 }
 
-// for(let i = 0; i < list.length; i++){
-//         previous.addEventListener('click', function(e){
-//         e.preventDefault()
-//         let countDown = nextCount
+function countUp() {
+    nextCount++
+    scrollLinks()
+    console.log(nextCount)
+    
+}
+next.addEventListener('click', countUp)
 
-//         countDown -= 1
-//         console.log(nextCount)
-//         if(navigations[0].style.display === 'none') {
-//             navigations[0].style.display = 'inline-block';
-//         }
-//         if(navigations[0].style.display !== 'none' && countDown === 1) {
-//             list[0].hidden = true;
-//             list[1].hidden = false;
-//         } if(countDown === 2) {
-//             list[1].hidden = true;
-//             list[2].hidden = false;
-//         } if(countDown === 3) {
-//             list[2].hidden = true;
-//             list[3].hidden = false;
-//         } if(countDown === 0) {
-//             navigations[0].style.display === 'none'
-//             list[0].hidden = false;
-//             list[1].hidden = true;
-//         }
-//         nextCount = countUp
-//     })
-// }
+function countDown() {
+    nextCount--
+    scrollLinks()
+    console.log(nextCount)
+}
+previous.addEventListener('click', countDown)
